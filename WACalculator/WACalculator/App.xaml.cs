d654120887c8f216f888844602057e68d8de2dbe
dhorbon <szymon080206@gmail.com>
2023-11-22 21:40:49 +0100

    initial

---
+++ WACalculator/WACalculator/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace WACalculator
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ WACalculator/WACalculator/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Text.Json;
+
+namespace WACalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        HttpClient client = new();
+
+        public MainPage()
+        {
+            InitializeComponent();
+            
+            client.BaseAddress = new Uri("http://api.wolframalpha.com/v1/");
+        }
+
+        private void Submit(object sender, EventArgs e)
+        {
+            Query query = new Query(input.Text);
+
+            var json = JsonSerializer.Serialize(query);
+
+            output.Text += json;
+
+
+            /*
+            StringContent stringContent = new StringContent(json);
+
+            output.Text += stringContent.ReadAsStringAsync().Result;
+
+            var response = client.PostAsync("result", stringContent).Result;
+            
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseContent = response.Content.ReadAsStringAsync().Result;
+
+                output.Text += responseContent;
+            }
+            else
+            {
+                output.Text += "error: " + response.StatusCode;
+                
+            }
+            */
+        }
+    }
+}
